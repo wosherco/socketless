686068d66e0160d11e1980ab3360fa62c4bb45f9
polvallverdu <pvallverdualcaide@gmail.com>
2024-08-31 12:27:04 +0200

    Fixed shadcn

--- apps/nextjs/tailwind.config.ts
+++ apps/nextjs/tailwind.config.ts
@@ -1,11 +1,12 @@
 import type { Config } from "tailwindcss";
-import baseConfig from "@socketless/tailwind-config/web";
 import { fontFamily } from "tailwindcss/defaultTheme";
+
+import baseConfig from "@socketless/tailwind-config/web";
 
 export default {
   // We need to append the path to the UI package to the content array so that
   // those classes are included correctly.
-  content: [...baseConfig.content, "../../packages/ui/src/*.{ts,tsx}"],
+  content: [...baseConfig.content, "../../internal-packages/ui/src/*.{ts,tsx}"],
   presets: [baseConfig],
   theme: {
     extend: {
