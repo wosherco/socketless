d970410868a950b7f2dad283a7aa90689a514cb6
polvallverdu <pvallverdualcaide@gmail.com>
2024-08-31 12:38:20 +0200

    Fixed create project form

--- apps/nextjs/src/components/forms/create-project-form.tsx
+++ apps/nextjs/src/components/forms/create-project-form.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import type { ReactNode } from "react";
 
 // import { useRouter } from "next/navigation";
@@ -13,88 +14,84 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@socketless/ui/dialog";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage, useForm } from "@socketless/ui/form";
+import { CreateProjectSchema } from "@socketless/validators";
+import { useRouter } from "next/navigation";
+import { api } from "~/trpc/react";
+import { toast } from "@socketless/ui/toast";
+import { Input } from "@socketless/ui/input";
+import { Button } from "@socketless/ui/button";
 
-// import {
-//   Form,
-//   FormControl,
-//   FormField,
-//   FormItem,
-//   FormLabel,
-//   FormMessage,
-//   useForm,
-// } from "@socketless/ui/form";
-// import { Input } from "@socketless/ui/input";
-// import { toast } from "@socketless/ui/toast";
-// import { CreateProjectSchema } from "@socketless/validators";
-
-// import { api } from "~/trpc/react";
 
 export function CreateProjectDialog({ children }: { children: ReactNode }) {
+  const [open, setOpen] = useState(false);
+
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger>{children}</DialogTrigger>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Create a Project</DialogTitle>
           <DialogDescription>
-            {/* Create a new project to start using Socketless. Note that billing is
-            attached to projects, not to your account. */}
-            Currently creating a project is not supported. If you want to try
-            Socketless, contact us at <b>contact@socketless.ws</b> or{" "}
-            <b>sales@socketless.ws</b>.
+            Create a new project to start using Socketless. Note that billing is
+            attached to projects, not to your account.
           </DialogDescription>
         </DialogHeader>
-        {/* <CreateProjectForm /> */}
+        <CreateProjectForm close={() => setOpen(false)} />
       </DialogContent>
     </Dialog>
   );
 }
 
-// export default function CreateProjectForm() {
-//   const form = useForm({
-//     schema: CreateProjectSchema,
-//     defaultValues: {
-//       name: "",
-//     },
-//   });
-//   const router = useRouter();
+export default function CreateProjectForm({ close }: { close?: () => void }) {
+  const form = useForm({
+    schema: CreateProjectSchema,
+    defaultValues: {
+      name: "",
+    },
+  });
+  const router = useRouter();
 
-//   const utils = api.useUtils();
-//   const createPost = api.projects.createProject.useMutation({
-//     onSuccess: async (data) => {
-//       await utils.projects.getProjects.invalidate();
-//       router.push(`/dashboard/${data.id}`);
-//     },
-//     onError: () => {
-//       toast.error("Failed to create project.");
-//     },
-//   });
+  const utils = api.useUtils();
+  const createPost = api.project.createProject.useMutation({
+    onSuccess: async (data) => {
+      if (close) {
+        close()
+      }
+      await utils.project.getProjects.invalidate();
+      router.push(`/dashboard/${data.id}`);
+    },
+    onError: () => {
+      toast.error("Failed to create project.");
+    },
+  });
 
-//   return (
-//     <Form {...form}>
-//       <form
-//         className="flex w-full max-w-2xl flex-col gap-4"
-//         onSubmit={form.handleSubmit((data) => {
-//           createPost.mutate(data);
-//         })}
-//       >
-//         <FormField
-//           control={form.control}
-//           name="name"
-//           render={({ field }) => (
-//             <FormItem>
-//               <FormLabel>Project Name</FormLabel>
-//               <FormControl>
-//                 <Input {...field} placeholder="Name" />
-//               </FormControl>
-//               <FormMessage />
-//             </FormItem>
-//           )}
-//         />
-//         <Button disabled={createPost.isPending} className="ml-auto w-fit">
-//           Create Project
-//         </Button>
-//       </form>
-//     </Form>
-//   );
-// }
+  return (
+    <Form {...form}>
+      <form
+        className="flex w-full max-w-2xl flex-col gap-4"
+        onSubmit={form.handleSubmit((data) => {
+          createPost.mutate(data);
+        })}
+      >
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Project Name</FormLabel>
+              <FormControl>
+                <Input {...field} placeholder="Name" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button disabled={createPost.isPending} className="ml-auto w-fit">
+          Create Project
+        </Button>
+      </form>
+    </Form>
+  );
+}
