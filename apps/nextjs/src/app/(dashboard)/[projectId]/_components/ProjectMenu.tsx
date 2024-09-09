d8e7dd99bfdf5da0ceccc0ae158e4783c9432044
polvallverdu <pvallverdualcaide@gmail.com>
2024-09-09 22:56:56 +0200

    Better menu

--- apps/nextjs/src/app/(dashboard)/[projectId]/_components/ProjectMenu.tsx
+++ apps/nextjs/src/app/(dashboard)/[projectId]/_components/ProjectMenu.tsx
@@ -22,10 +22,12 @@ function ProjectMenuButton({
   children,
   icon,
   href,
+  newtab
 }: {
   children: React.ReactNode;
   icon: React.ReactNode;
   href: string;
+  newtab?: boolean;
 }) {
   const currentPathname = usePathname();
   const [marked, setMarked] = React.useState(false);
@@ -45,6 +47,7 @@ function ProjectMenuButton({
         "flex flex-row items-center rounded-lg px-4 py-2 transition-colors hover:bg-accent disabled:opacity-50",
         marked && "bg-accent",
       )}
+      target={newtab ? "_blank" : undefined}
     >
       <span className="md:mr-2">{icon}</span>{" "}
       <span className="hidden text-sm font-medium md:block">{children}</span>
@@ -116,11 +119,12 @@ export default function ProjectMenu({ projectId }: { projectId: number }) {
       <ProjectMenuButton
         icon={<Book width={20} />}
         href="https://docs.socketless.ws"
+        newtab
       >
         Docs
       </ProjectMenuButton>
 
-      <ProjectMenuButton icon={<Info width={20} />} href={`/#start`}>
+      <ProjectMenuButton icon={<Info width={20} />} href="mailto:support@socketless.ws" newtab>
         Support
       </ProjectMenuButton>
     </div>
