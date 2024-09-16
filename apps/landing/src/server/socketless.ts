21f2e665dfaff97b9cd4f65e9c891313e6e85c02
polvallverdu <pvallverdualcaide@gmail.com>
2024-09-16 13:46:10 +0200

    Added validation

--- apps/landing/src/server/socketless.ts
+++ apps/landing/src/server/socketless.ts
@@ -1,4 +1,5 @@
 import { createSocketless } from "socketless.ws/server";
+import { z } from "zod";
 
 import { env } from "~/env";
 
@@ -6,6 +7,7 @@ export const socketless = createSocketless<string, string>({
   clientId: env.SOCKETLESS_CLIENT_ID,
   token: env.SOCKETLESS_TOKEN,
   url: env.SOCKETLESS_URL,
+  messageValidator: z.string(),
   onConnect(context, identifier) {
     context.send(`${identifier} connected`, { feeds: ["landing"] });
   },
