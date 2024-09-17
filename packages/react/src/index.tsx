fc9ca13004615f9e60247550c384c7ccf65c4e70
polvallverdu <pvallverdualcaide@gmail.com>
2024-09-17 16:49:10 +0200

    Fixed formatting

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,4 +1,10 @@
-import React, { createContext, useContext, useEffect, useRef, useState } from "react";
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { SocketlessWebsocket } from "socketless.ws/client";
 
 import type { WebsocketMessage } from "@socketless/shared";
