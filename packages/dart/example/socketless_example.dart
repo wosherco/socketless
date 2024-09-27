46e12a7c844162957aa3e2cd2fbcd5f2189a593c
polvallverdu <pvallverdualcaide@gmail.com>
2024-09-27 23:22:58 +0200

    Initial Dart client

---
+++ packages/dart/example/socketless_example.dart
@@ -0,0 +1,6 @@
+import 'package:socketless_dart/socketless.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/dart/test/socketless_test.dart
@@ -0,0 +1,16 @@
+import 'package:socketless_dart/socketless.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
