e2b12afc3a4312b9e90b8589420005a35b3f5182
polvallverdu <pvallverdualcaide@gmail.com>
2024-09-30 00:38:48 +0200

    Added example

--- packages/dart/example/socketless_example.dart
+++ packages/dart/example/socketless_example.dart
@@ -1,6 +1,28 @@
+import 'dart:convert';
+
 import 'package:socketless_dart/socketless.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  // Initialize the client
+  final client = SocketlessClient(
+    url: 'your socketless url',
+    onMessage: (message) => print(message),
+    onStateChange: (newState) => print("New Websocket State: $newState"),
+  );
+
+  // Sending a message
+  client.send("Hello!");
+
+  client.send(jsonEncode({
+    "message": "json also works :D",
+  }));
+
+  // Update the url
+  client.updateUrl("new socketless url");
+
+  // Disconnect the client
+  client.disconnect();
+
+  // Close the client (will terminate and clean up the client, you won't be able to use it again)
+  client.close();
 }
