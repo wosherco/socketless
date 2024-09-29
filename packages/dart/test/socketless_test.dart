0cd94bec007c86188e4aae6ab2985f940a3cb1af
polvallverdu <pvallverdualcaide@gmail.com>
2024-09-30 00:38:56 +0200

    Commented test

--- packages/dart/test/socketless_test.dart
+++ packages/dart/test/socketless_test.dart
@@ -2,15 +2,16 @@ import 'package:socketless_dart/socketless.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
+  // TODO: Tests
+  // group('A group of tests', () {
+  //   final awesome = Awesome();
 
-    setUp(() {
-      // Additional setup goes here.
-    });
+  //   setUp(() {
+  //     // Additional setup goes here.
+  //   });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
-  });
+  //   test('First Test', () {
+  //     expect(awesome.isAwesome, isTrue);
+  //   });
+  // });
 }
